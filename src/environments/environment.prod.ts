f455cb9c43a0f770d954ac772e32e7b94c01f761
Martaarmu <amarjona@iesfranciscodelosrios.es>
2021-11-07 11:54:20 +0100

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyBqunpdSzmNDk2KXU7VADSgyJz158PMjy8",
+    authDomain: "comprasapp-56af0.firebaseapp.com",
+    databaseURL: "https://comprasapp-56af0-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "comprasapp-56af0",
+    storageBucket: "comprasapp-56af0.appspot.com",
+    messagingSenderId: "432167034954",
+    appId: "1:432167034954:web:56ec49328075d1a42f86b3"
+  }
 };
